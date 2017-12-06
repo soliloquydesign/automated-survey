09fd30c261fd393a0d52d2123810ea9360fec153
Tyler <soliloquydesign@gmail.com>
2017-12-05 19:32:34 -0800

    alter last q, delete bears, created BMI converter

--- automated_survey/util.py
+++ automated_survey/util.py
@@ -15,3 +15,19 @@ class SurveyLoader(object):
                      for question in self.survey['questions']]
         new_survey.save()
         new_survey.question_set.add(*questions)
+
+
+def BMIConverter(height,weight):
+    #extract height
+    extract_height_feet = height.split("'")
+    extract_height_inches = extract_height_feet[1].split('"')
+    extracted_height_pair = [extract_height_feet[0], extract_height_inches[0]]
+
+    #convert extracted height into meters
+    inches_percent_of_foot = float(extracted_height_pair[1]) / 12
+    total_height_in_feet = float(extracted_height_pair[0]) + inches_percent_of_foot
+    total_height_in_metric = total_height_in_feet * .3048
+
+    #convert extracted weight to kilograms
+    total_weight_in_metric = float(weight) *.453592
+    return (total_weight_in_metric/total_height_in_metric)/total_height_in_metric
